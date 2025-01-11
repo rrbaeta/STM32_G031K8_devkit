ca84f4320f2091c7b823e42bc703cb650e242a4d
Rodrigo Baeta <baettahimself@gmail.com>
2025-01-11 22:19:24 +0000

    AC valve test for programmer

--- src/main.cpp
+++ src/main.cpp
@@ -1,153 +1,46 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h>
-#include <Wire.h>
 
-#include "config.hpp"
-#include "isr.hpp"
-#include "buttons.hpp"
+#define IN1_PIN PB2 // Pin connected to IN1 of H-Bridge
+#define IN2_PIN PA8 // Pin connected to IN2 of H-Bridge
 
-// variable declarations
-static bool flag_10_ms = false;
-static bool flag_100_ms = false;
-static bool flag_1_s = false;
+// Define AC frequency (50Hz or 60Hz)
+#define AC_FREQUENCY 50 // Change to 60 if needed
 
-// Peripherals
-HardwareSerial serial();
-LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_EN, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
+// Calculate PWM period in milliseconds for the given frequency
+#define PWM_PERIOD_MS (1000 / (2 * AC_FREQUENCY)) // Half-period duration in ms
 
-// function prototypes
-void set_local_timer_flags(void);
-void flash_led(void);
-void operate_leds(void);
+// Define ON and OFF durations in milliseconds
+#define ON_DURATION_MS 1000
+#define OFF_DURATION_MS 1000
 
 void setup()
 {
-  gpio_config();
-
-  digitalWrite(PC6, LOW);
-
-  delay(1000);
-
-  // Uart
-  Serial.begin(9600);
-  Serial.println("+++++Init+++++");
-
-  isr_init();
-
-  // digitalWrite(PB7, false);
-
-  lcd.begin(16, 2);
-  lcd.home();
-
-  lcd.setCursor(0, 0); /* Set cursor to column 0 row 0 */
-  lcd.print("Boa Noite");
+    // Set H-Bridge control pins as outputs
+    pinMode(IN1_PIN, OUTPUT);
+    pinMode(IN2_PIN, OUTPUT);
 }
 
 void loop()
 {
-  set_local_timer_flags();
+    // Step 1: Generate the VAC signal for 5 seconds
+    unsigned long startTime = millis(); // Record the start time
+    while (millis() - startTime < ON_DURATION_MS)
+    {
+        // Generate square wave signals for the H-Bridge
 
-  // Read Inputs
-  if (flag_10_ms)
-  {
-    read_buttons();
-  }
+        // Forward (IN1 = HIGH, IN2 = LOW)
+        digitalWrite(IN1_PIN, HIGH);
+        digitalWrite(IN2_PIN, LOW);
+        delay(PWM_PERIOD_MS); // Wait for half-period
 
-  if (flag_100_ms)
-  {
-  }
+        // Reverse (IN1 = LOW, IN2 = HIGH)
+        digitalWrite(IN1_PIN, LOW);
+        digitalWrite(IN2_PIN, HIGH);
+        delay(PWM_PERIOD_MS); // Wait for half-period
+    }
 
-  if (flag_1_s)
-  {
-  }
-
-  // Calculations
-  if (flag_10_ms)
-  {
-  }
-
-  if (flag_100_ms)
-  {
-  }
-
-  if (flag_1_s)
-  {
-  }
-
-  // Set outputs
-  if (flag_10_ms)
-  {
-    operate_leds();
-  }
-
-  if (flag_100_ms)
-  {
-    // flash_led();
-  }
-
-  if (flag_1_s)
-  {
-  }
-
-  // //reset timer flags
-  flag_10_ms = false;
-  flag_100_ms = false;
-  flag_1_s = false;
-}
-
-void set_local_timer_flags(void)
-{
-  static bool old_10_ms = false;
-  static bool old_100_ms = false;
-  static bool old_1_s = false;
-
-  // set timer flags so they are not reset mid loop
-
-  if (old_10_ms != timers.t_10_ms)
-  {
-    old_10_ms = !old_10_ms;
-    flag_10_ms = true;
-  }
-
-  if (old_100_ms != timers.t_100_ms)
-  {
-    old_100_ms = !old_100_ms;
-    flag_100_ms = true;
-  }
-
-  if (old_1_s != timers.t_1_s)
-  {
-    old_1_s = !old_1_s;
-    flag_1_s = true;
-  }
-}
-
-void flash_led(void)
-{
-  static bool LED_state = false;
-
-  digitalWrite(PC6, !LED_state);
-
-  LED_state = !LED_state;
-}
-
-void operate_leds(void)
-{
-  if (button_1.toggle_press)
-  {
-    button_1.toggle_press = false;
-  }
-
-  digitalWrite(ON_BOARD_LED, !digitalRead(BTN_1));
-
-  if (button_1.short_press)
-  {
-    lcd.setCursor(0, 1); /* Set cursor to column 0 row 0 */
-    lcd.print("Hey");
-  }
-
-  if (button_1.long_press)
-  {
-    lcd.clear();
-  }
+    // Step 2: Stop the signal for 5 seconds
+    digitalWrite(IN1_PIN, LOW); // Set both pins LOW
+    digitalWrite(IN2_PIN, LOW); // This puts the H-Bridge in coast mode
+    delay(OFF_DURATION_MS);     // Wait for 5 seconds
 }
---
+++ src/main_2.cpp
@@ -0,0 +1,153 @@
+// #include <Arduino.h>
+// #include <LiquidCrystal.h>
+// #include <Wire.h>
+
+// #include "config.hpp"
+// #include "isr.hpp"
+// #include "buttons.hpp"
+
+// // variable declarations
+// static bool flag_10_ms = false;
+// static bool flag_100_ms = false;
+// static bool flag_1_s = false;
+
+// // Peripherals
+// HardwareSerial serial();
+// LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_EN, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
+
+// // function prototypes
+// void set_local_timer_flags(void);
+// void flash_led(void);
+// void operate_leds(void);
+
+// void setup()
+// {
+//   gpio_config();
+
+//   digitalWrite(PC6, LOW);
+
+//   delay(1000);
+
+//   // Uart
+//   Serial.begin(9600);
+//   Serial.println("+++++Init+++++");
+
+//   isr_init();
+
+//   // digitalWrite(PB7, false);
+
+//   lcd.begin(16, 2);
+//   lcd.home();
+
+//   lcd.setCursor(0, 0); /* Set cursor to column 0 row 0 */
+//   lcd.print("Boa Noite");
+// }
+
+// void loop()
+// {
+//   set_local_timer_flags();
+
+//   // Read Inputs
+//   if (flag_10_ms)
+//   {
+//     read_buttons();
+//   }
+
+//   if (flag_100_ms)
+//   {
+//   }
+
+//   if (flag_1_s)
+//   {
+//   }
+
+//   // Calculations
+//   if (flag_10_ms)
+//   {
+//   }
+
+//   if (flag_100_ms)
+//   {
+//   }
+
+//   if (flag_1_s)
+//   {
+//   }
+
+//   // Set outputs
+//   if (flag_10_ms)
+//   {
+//     operate_leds();
+//   }
+
+//   if (flag_100_ms)
+//   {
+//     // flash_led();
+//   }
+
+//   if (flag_1_s)
+//   {
+//   }
+
+//   // //reset timer flags
+//   flag_10_ms = false;
+//   flag_100_ms = false;
+//   flag_1_s = false;
+// }
+
+// void set_local_timer_flags(void)
+// {
+//   static bool old_10_ms = false;
+//   static bool old_100_ms = false;
+//   static bool old_1_s = false;
+
+//   // set timer flags so they are not reset mid loop
+
+//   if (old_10_ms != timers.t_10_ms)
+//   {
+//     old_10_ms = !old_10_ms;
+//     flag_10_ms = true;
+//   }
+
+//   if (old_100_ms != timers.t_100_ms)
+//   {
+//     old_100_ms = !old_100_ms;
+//     flag_100_ms = true;
+//   }
+
+//   if (old_1_s != timers.t_1_s)
+//   {
+//     old_1_s = !old_1_s;
+//     flag_1_s = true;
+//   }
+// }
+
+// void flash_led(void)
+// {
+//   static bool LED_state = false;
+
+//   digitalWrite(PC6, !LED_state);
+
+//   LED_state = !LED_state;
+// }
+
+// void operate_leds(void)
+// {
+//   if (button_1.toggle_press)
+//   {
+//     button_1.toggle_press = false;
+//   }
+
+//   digitalWrite(ON_BOARD_LED, !digitalRead(BTN_1));
+
+//   if (button_1.short_press)
+//   {
+//     lcd.setCursor(0, 1); /* Set cursor to column 0 row 0 */
+//     lcd.print("Hey");
+//   }
+
+//   if (button_1.long_press)
+//   {
+//     lcd.clear();
+//   }
+// }
